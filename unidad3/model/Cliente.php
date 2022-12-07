d07e008e96e67439157b3a8bf4702019966f33e7
blame110 <46011863+blame110@users.noreply.github.com>
2022-12-07 14:10:35 +0100

    Modelo Cliente AcessoBD

---
+++ unidad3/model/Cliente.php
@@ -0,0 +1,194 @@
+<?php
+
+require_once("Utils.php");
+
+class Cliente
+{
+
+    /**
+     * Nos devuelve los clientes mayores de la edad minima con el sexo introducido
+     */
+    public function getClienteSel($edadMin, $sexo, $conexPDO)
+    {
+
+        if (isset($edadMin) && is_numeric($edadMin) && isset($sexo) && is_string($sexo)) {
+
+
+            if ($conexPDO != null) {
+
+                try {
+                    //Primero introducimos la sentencia a ejecutar con prepare
+                    //Ponemos en lugar de valores directamente, interrogaciones
+                    $sentencia = $conexPDO->prepare("SELECT * FROM tienda.clientes where edad>:edad and sexo=:sexo");
+                    //Asociamos a cada interrogacion el valor que queremos en su lugar
+                    $sentencia->bindParam(":edad", $edadMin);
+                    $sentencia->bindParam(":sexo", $sexo);
+                    //Ejecutamos la sentencia
+                    $sentencia->execute();
+
+                    //Devolvemos los datos del cliente
+                    return $sentencia->fetchAll();
+                } catch (PDOException $e) {
+                    print("Error al Acceder a BD" . $e->getMessage());
+                }
+            }
+        }
+    }
+
+    /**Funcion que nos devuelve todos los clientes */
+    public function getClientes($conexPDO, $orden, $campoOrd, $numPag, $cantElem)
+    {
+
+        if ($conexPDO != null) {
+            try {
+                //Primero introducimos la sentencia a ejecutar con prepare
+                //Ponemos en lugar de valores directamente, interrogaciones
+                $sentencia = $conexPDO->prepare("SELECT * FROM tienda.clientes");
+                //Ejecutamos la sentencia
+                $sentencia->execute();
+
+                //Devolvemos los datos del cliente
+                return $sentencia->fetchAll();
+            } catch (PDOException $e) {
+                print("Error al acceder a BD" . $e->getMessage());
+            }
+        }
+    }
+
+    /**
+     * Devuelve el cliente asociado a la clave primaria introducida
+     */
+    public function getCliente($idCliente, $conexPDO)
+    {
+        if (isset($idCliente) && is_numeric($idCliente)) {
+
+
+            if ($conexPDO != null) {
+                try {
+                    //Primero introducimos la sentencia a ejecutar con prepare
+                    //Ponemos en lugar de valores directamente, interrogaciones
+                    $sentencia = $conexPDO->prepare("SELECT * FROM tienda.clientes where idClientes=?");
+                    //Asociamos a cada interrogacion el valor que queremos en su lugar
+                    $sentencia->bindParam(1, $idCliente);
+                    //Ejecutamos la sentencia
+                    $sentencia->execute();
+
+                    //Devolvemos los datos del cliente
+                    return $sentencia->fetch();
+                } catch (PDOException $e) {
+                    print("Error al acceder a BD" . $e->getMessage());
+                }
+            }
+        }
+    }
+
+    function addCliente($cliente, $conexPDO)
+    {
+
+        $result = null;
+           if (isset($cliente) && $conexPDO != null) {
+
+            try {
+                //Preparamos la sentencia
+                $sentencia = $conexPDO->prepare("INSERT INTO tienda.clientes (nombre, email, edad,sexo) VALUES ( :nombre, :email, :edad, :sexo)");
+
+                //Asociamos los valores a los parametros de la sentencia sql
+                $sentencia->bindParam(":nombre", $cliente["nombre"]);
+                $sentencia->bindParam(":email", $cliente["email"]);
+                $sentencia->bindParam(":edad", $cliente["edad"]);
+                $sentencia->bindParam(":sexo", $cliente["sexo"]);
+
+                //Ejecutamos la sentencia
+                $result = $sentencia->execute();
+            } catch (PDOException $e) {
+                print("Error al acceder a BD" . $e->getMessage());
+            }
+        }
+
+        return $result;
+    }
+
+    function delCliente($idCliente, $conexPDO)
+    {
+        $result=null;
+
+        if (isset($idCliente) && is_numeric($idCliente)) {
+
+
+            if ($conexPDO != null) {
+                try {
+                   //Borramos el cliente asociado a dicho id
+                    $sentencia = $conexPDO->prepare("DELETE  FROM tienda.clientes where idClientes=?");
+                    //Asociamos a cada interrogacion el valor que queremos en su lugar
+                    $sentencia->bindParam(1, $idCliente);
+                    //Ejecutamos la sentencia
+                    $result = $sentencia->execute();
+
+
+                } catch (PDOException $e) {
+                    print("Error al borrar" . $e->getMessage());
+                }
+            }
+        }
+
+        return $result;
+
+    }
+
+    function updateCliente($cliente,$conexPDO)
+    {
+        
+        $result = null;
+           if (isset($cliente) && isset($cliente["idCliente"]) && is_numeric($cliente["idCliente"])  && $conexPDO != null) {
+
+            try {
+                //Preparamos la sentencia
+                $sentencia = $conexPDO->prepare("UPDATE tienda.clientes set nombre=:nombre, email=:email, edad=:edad,sexo=:sexo where idClientes=:idCliente");
+
+                //Asociamos los valores a los parametros de la sentencia sql
+                $sentencia->bindParam(":idCliente", $cliente["idCliente"]);
+                $sentencia->bindParam(":nombre", $cliente["nombre"]);
+                $sentencia->bindParam(":email", $cliente["email"]);
+                $sentencia->bindParam(":edad", $cliente["edad"]);
+                $sentencia->bindParam(":sexo", $cliente["sexo"]);
+
+                print ($sentencia::queryString);
+
+                //Ejecutamos la sentencia
+                $result = $sentencia->execute();
+            } catch (PDOException $e) {
+                print("Error al acceder a BD" . $e->getMessage());
+            }
+        }
+
+        return $result;
+    }
+
+}
+
+$gestorCli = new Cliente();
+
+//Nos conectamos a la Bd
+$conexPDO = Utils::conectar();
+
+//$gestorCli->getCliente(1,$conexPDO);
+$resultado = $gestorCli->getClienteSel(18, "M", $conexPDO);
+
+$resultado2 = $gestorCli->getClientes($conexPDO, null, null, null, null);
+
+//var_dump($resultado2);
+
+print("El nombre de la segunda mujer es" . $resultado[1]["nombre"]);
+
+$alvaro = ["nombre" => "alvaro", "email" => "alvaro@gmail.com", "edad" => 24, "sexo" => "H"];
+//Probamos la insercion
+var_dump($gestorCli->addCliente($alvaro, $conexPDO));
+
+//Modificamos la edad de alvaro
+$alvaro["edad"]=13;
+$alvaro["idCliente"]=8;
+
+var_dump($gestorCli->updateCliente($alvaro, $conexPDO));
+
+
+//var_dump($gestorCli->delCliente(6,$conexPDO));
